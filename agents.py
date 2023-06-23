170fb00020a22fd727d6368f3b6ece3c834bacf5
dntAtMe <dntatm3@gmail.com>
2023-06-24 01:19:56 +0200

    Spawn and step changes

--- agents.py
+++ agents.py
@@ -16,9 +16,19 @@ class Ant(mesa.Agent):
       moore=True,
       include_center=False
     )
+
     new_position = random.choice(possible_steps)
     self.model.grid.move_agent(self, new_position)
 
 class Food(mesa.Agent):
-  def __init__(self, unique_id: int, model: mesa.Model) -> None:
+  def __init__(self, unique_id: int, model: mesa.Model, steps_valid: int) -> None:
     super().__init__(unique_id, model)
+
+    self.steps_valid = steps_valid
+
+  def step(self) -> None:
+    if self.steps_valid <= 0:
+      self.model.grid.remove_agent(self)
+      self.model.schedule.remove(self)
+    else:
+      self.steps_valid -= 1
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from typing import Any
 import mesa
+import random
 from agents import Ant, Food
 from mesa.visualization.modules import CanvasGrid
 
-GRID_SIZE = 25
+GRID_SIZE = 50
 
 class AntFarmModel(mesa.Model):
   def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -11,19 +12,23 @@ class AntFarmModel(mesa.Model):
 
     self.grid = mesa.space.MultiGrid(GRID_SIZE, GRID_SIZE, torus=True)
     self.schedule = mesa.time.SimultaneousActivation(self)
+    self.current_step = 0
 
-    for i in range(10):
+    for i in range(20):
       ant = Ant(i, self)
-      x = self.random.randrange(self.grid.width)
-      y = self.random.randrange(self.grid.height)
+      x = self.random.randrange(self._get_grid_center()['x'] - 10, self._get_grid_center()['x'] + 10)
+      y = self.random.randrange(self._get_grid_center()['y'] - 10, self._get_grid_center()['y'] + 10)
       self.grid.place_agent(ant, (x, y))
       self.schedule.add(ant)
 
     for _ in range(10):
       self._spawn_food_at_random_location()
 
+  def _get_grid_center(self) -> dict:
+    return { 'x': self.grid.width / 2, 'y': self.grid.height / 2 }
+
   def _spawn_food_at_random_location(self) -> None:
-    food = Food(self._last_agent_id() + 1, self)
+    food = Food(self._last_agent_id() + 1, self, random.randrange(25, 50))
     x = self.random.randrange(self.grid.width)
     y = self.random.randrange(self.grid.height)
     self.grid.place_agent(food, (x, y))
@@ -33,11 +38,15 @@ class AntFarmModel(mesa.Model):
     return max(self.schedule._agents.keys())
 
   def step(self) -> None:
+    self.current_step += 1
+    if self.current_step % 5 == 0:
+      self._spawn_food_at_random_location()
+
     self.schedule.step()
 
 def agent_portrayal(agent: mesa.Agent) -> dict:
   if agent is None:
-    return
+    return {}
   
   portrayal = {}
 
@@ -52,7 +61,7 @@ def agent_portrayal(agent: mesa.Agent) -> dict:
     portrayal["Filled"] = "true"
     portrayal["Layer"] = 0
     portrayal["Color"] = "green"
-    portrayal["r"] = 0.5
+    portrayal["r"] = 0.35
 
   return portrayal
 
